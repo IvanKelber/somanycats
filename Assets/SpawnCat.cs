1db2e3ef1f2a566b4d3e0efece959d972b734a83
Ivan Kelber <ivan_kelber@brown.edu>
2016-02-06 18:44:30 -0500

    added script

---
+++ Assets/SpawnCat.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnCat : MonoBehaviour {
+
+	public GameObject cat;
+	public float timer = 0.0f;
+	public Vector3 spawn_position;
+
+	void spawnCat(){
+		spawn_position = new Vector3 (Random.Range (-2, 2), 1, Random.Range (-10, 10));
+		Instantiate (cat, spawn_position, Quaternion.identity);
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer > 5) {
+			spawnCat();
+			timer = 0;
+		}
+	}
+}
