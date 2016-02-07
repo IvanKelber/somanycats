85c12e73ede088a896ddeab47e6691ac01393ff7
Rebecca Elyanow <relyanow@Rebeccas-MacBook-Pro-2.local>
2016-02-06 20:43:21 -0500

    added stuff

--- Assets/SpawnCat.cs
+++ Assets/SpawnCat.cs
@@ -9,7 +9,10 @@ public class SpawnCat : MonoBehaviour {
 
 	void spawnCat(){
 		spawn_position = new Vector3 (Random.Range (-2, 2), 1, Random.Range (-10, 10));
-		Instantiate (cat, spawn_position, Quaternion.identity);
+		GameObject tmpcat = Instantiate (cat, spawn_position, transform.rotation*Quaternion.Euler(180,180,0)) as GameObject;
+		//tmpcat.transform.Rotate (90, 0, 0);
+		tmpcat.tag = "cat";
+		//tmpcat.AddComponent<MovementScript>();
 	}
 	// Use this for initialization
 	void Start () {
@@ -19,9 +22,10 @@ public class SpawnCat : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		timer += Time.deltaTime;
-		if (timer > 5) {
+		if (timer > 3) {
 			spawnCat();
 			timer = 0;
+
 		}
 	}
 }
