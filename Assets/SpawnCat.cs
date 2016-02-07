9146fb71ff97e586fc7269d0bdb8e43d5b4d76ba
Ivan Kelber <ivan_kelber@brown.edu>
2016-02-06 20:53:28 -0500

    hopefully this works

--- Assets/SpawnCat.cs
+++ Assets/SpawnCat.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class SpawnCat : MonoBehaviour {
 
-	public GameObject cat;
+	public GameObject[] cats = new GameObject[3];
 	public float timer = 0.0f;
 	public Vector3 spawn_position;
 
 	void spawnCat(){
 		spawn_position = new Vector3 (Random.Range (-2, 2), 1, Random.Range (-10, 10));
-		GameObject tmpcat = Instantiate (cat, spawn_position, transform.rotation*Quaternion.Euler(180,180,0)) as GameObject;
+		GameObject tmpcat = Instantiate (cats[Random.Range(0,3)], spawn_position, transform.rotation*Quaternion.Euler(180,180,0)) as GameObject;
 		//tmpcat.transform.Rotate (90, 0, 0);
 		tmpcat.tag = "cat";
 		//tmpcat.AddComponent<MovementScript>();
@@ -22,7 +22,7 @@ public class SpawnCat : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		timer += Time.deltaTime;
-		if (timer > 3) {
+		if (timer > 1) {
 			spawnCat();
 			timer = 0;
 
